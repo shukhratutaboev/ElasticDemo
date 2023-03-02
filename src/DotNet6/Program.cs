1e41398734902b2ca597612ec86d8f3b6c4a83be
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2023-03-02 12:26:24 +0500

    Some configurations

--- src/DotNet6/Program.cs
+++ src/DotNet6/Program.cs
@@ -1,6 +1,9 @@
+using DotNet6;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog(Serilogger.Configure);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,11 +13,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
---
+++ src/DotNet6/Serilogger.cs
@@ -0,0 +1,35 @@
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+
+namespace DotNet6;
+
+public static class Serilogger
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+        (context, configuration) =>
+        {
+            var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+
+            configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(elasticUri))
+                    {
+                        IndexFormat = $"{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                        AutoRegisterTemplate = true,
+                        ModifyConnectionSettings = connection => connection
+                            .DisableAutomaticProxyDetection()
+                            .BasicAuthentication(context.Configuration["ElasticConfiguration:Username"], context.Configuration["ElasticConfiguration:Password"]),
+                        NumberOfShards = 2,
+                        NumberOfReplicas = 1
+                    })
+                .ReadFrom.Configuration(context.Configuration);
+        };
+}
